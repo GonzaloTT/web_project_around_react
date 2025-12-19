d266b6f4b89d6292129662520ee2779cabdab812
Gonzalo <maganagonzalosegovia@gmail.com>
2025-12-19 12:00:31 -0600

    Se agrego marcado JSX y carpetas img, vendor, y blocks para estilos

--- around_react/src/App.jsx
+++ around_react/src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "../images/Around.svg";
+import vector from "../images/Vector.svg";
+import person from "../images/Person.png";
+import editButton from "../images/Edit_Button.svg";
+import addButton from "../images/Add_Button.svg";
+import footer from "../images/Footer.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="page">
+      <header className="header">
+        <img
+          src={logo}
+          alt="Around_The_U.S."
+          className="header__title"
+        />
+        <img src={vector} alt="Vector" className="header__element" />
+      </header>
+      <div class="content">
+      <section className="content__profile">
+          <div className="content__avatar_container">
+            <img
+              src={person}
+              alt="Person"
+              className="content__image content__avatar"
+            />
+            <button
+              className="content__button content__button_avatar_edit"
+            ></button>
+          </div>
+          <div className="content__info">
+            <h1 className="content__name">Jacques Cousteau</h1>
+            <h4 className="content__job">Explorador</h4>
+          </div>
+          <button className="content__button content__button_edit">
+            <img
+              src={editButton}
+              alt="Edit_Button"
+              className="content__button content__button_edit_image"
+            />
+          </button>
+          <button className="content__button content__button_add">
+            <img
+              src={addButton}
+              alt="Add_button"
+              className="content__button content__button_add_image"
+            />
+          </button>
+        </section>
+        <footer class="footer">
+        <img src={footer} alt="Footer" className="footer__image" />
+        </footer>
+        </div>
+        </div>
     </>
   )
 }
